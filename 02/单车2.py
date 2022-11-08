7291d3c86025b980fdec297ec64a409e507e08d1
tai <t385633933@163.com>
2022-11-08 10:09:34 +0800

    ok

--- 02/单车2.py
+++ 02/单车2.py
@@ -18,8 +18,7 @@ for each in dummy_fields:
     #将新的独热编码变量与原有的所有变量合并到一起
     rides = pd.concat([rides, dummies], axis=1)
 #将原来的类型变量从数据表中删除
-fields_to_drop = ['instant', 'dteday', 'season', 'weathersit', 'weekday', 'atemp', 'mnth', 'workingday',
-'hr'] #要删除的类型变量的名称
+fields_to_drop = ['instant', 'dteday', 'season', 'weathersit', 'weekday', 'atemp', 'mnth', 'workingday','hr'] #要删除的类型变量的名称
 data = rides.drop(fields_to_drop, axis=1) #将它们从数据库的变量中删除
 
 
@@ -111,3 +110,51 @@ dates = dates.apply(lambda d: d.strftime('%b %d'))
 ax.set_xticks(np.arange(len(dates))[12::24])
 _ = ax.set_xticklabels(dates[12::24], rotation=45)
 plt.show()
+
+
+
+def feature(X, net):
+    #定义一个函数，用于提取网络的权重信息，所有的网络参数信息全部存储在neu的named_parameters集合中
+    X = torch.from_numpy(X).type(torch.FloatTensor)
+    dic = dict(net.named_parameters()) #提取这个集合
+    weights = dic['0.weight'] #可以按照“层数.名称”来索引集合中的相应参数值
+    biases = dic['0.bias']
+    h = torch.sigmoid(X.mm(weights.t()) + biases.expand([len(X), len(biases)])) #隐含层的计算过程
+    return h #输出层的计算
+
+
+bool1 = rides['dteday'] == '2012-12-22'
+bool2 = rides['dteday'] == '2012-12-23'
+bool3 = rides['dteday'] == '2012-12-24'
+#将3个布尔型数组求与
+bools = [any(tup) for tup in zip(bool1,bool2,bool3) ]
+#将相应的变量取出来
+subset = test_features.loc[rides[bools].index]
+subtargets = test_targets.loc[rides[bools].index]
+subtargets = subtargets['cnt']
+subtargets = subtargets.values.reshape([len(subtargets),1])
+
+
+#将数据输入到神经网络中，读取隐含层神经元的激活数值，存入results中
+results = feature(subset.values, neu).data.numpy()
+#这些数据对应的预测值（输出层）
+predict = neu(torch.FloatTensor(subset.values)).data.numpy()
+#将预测值还原为原始数据的数值范围
+mean, std = scaled_features['cnt']
+predict = predict * std + mean
+subtargets = subtargets * std + mean
+
+
+#将所有的神经元激活水平画在同一张图上
+fig, ax = plt.subplots(figsize = (8, 6))
+ax.plot(results[:,:],'.:',alpha = 0.1)
+ax.plot((predict - min(predict)) / (max(predict) - min(predict)),'bo-',label='Prediction')
+ax.plot((subtargets - min(predict)) / (max(predict) - min(predict)),'ro-',label='Real')
+ax.plot(results[:, 5],'.:',alpha=1,label='Neuro 6')
+ax.set_xlim(right=len(predict))
+ax.legend()
+plt.ylabel('Normalized Values')
+dates = pd.to_datetime(rides.loc[subset.index]['dteday'])
+dates = dates.apply(lambda d: d.strftime('%b %d'))
+ax.set_xticks(np.arange(len(dates))[12::24])
+_ = ax.set_xticklabels(dates[12::24], rotation=45)
