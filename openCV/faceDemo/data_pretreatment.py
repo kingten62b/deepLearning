66e717ec90b8b0c2e211181f64ed2a898bf09b26
tai <t385633933@163.com>
2022-12-05 09:16:48 +0800

    优化数据集加载器

--- openCV/faceDemo/data_pretreatment.py
+++ openCV/faceDemo/data_pretreatment.py
@@ -18,7 +18,7 @@ def get_transform():
             # 对每个像素点进行归一化
             # transforms.Normalize(mean=[0.4, 0.4, 0.4], # 均值
             #                      std=[0.2, 0.2, 0.2])  # 方差
-            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))# 均值 方差
         ])
 
 def get_dataset(batch_size=10, num_workers=config.NUM_WORKERS):
--- openCV/faceDemo/train.py
+++ openCV/faceDemo/train.py
@@ -1,4 +1,3 @@
-import os
 import config
 import torch
 from data_pretreatment import get_dataset
@@ -13,7 +12,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def train_model():
     record = [] #记录训练数据集准确率/测试集准确率的容器,用于后续绘图
-    train_loader, test_loader = get_dataset(batch_size=config.BATCH_SIZE)
+    train_loader, validation_loader, test_loader = get_dataset(batch_size=config.BATCH_SIZE)
     net = Net().to(DEVICE)
     # 使用Adam/SDG优化器
     # optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
@@ -37,15 +36,15 @@ def train_model():
             if (step + 1) % 5 == 0:
                 train_rights.append(rightness(output, y)) #将计算结果装到列表容器train_rights中
                 train_r = (sum([tup[0] for tup in train_rights]), sum([tup[1] for tup in train_rights]))
-                #开始在测试集上做循环，计算测试集上的准确度
-                val_rights = [] #记录测试集准确率的容器
-                for (x, y) in test_loader:
+                #开始在验证集上做循环，计算验证集上的准确度
+                val_rights = [] #记录验证集准确率的容器
+                for (x, y) in validation_loader:
                     x, y = x.to(DEVICE), y.to(DEVICE)
                     output = net(x)
                     val_rights.append(rightness(output, y)) #将计算结果装到列表容器val_rights中
                 val_r = (sum([tup[0] for tup in val_rights]), sum([tup[1] for tup in val_rights]))
 
-                print('训练周期: {} [{:.0f}%]\tLoss: {:.6f}\t训练集正确率: {:.3f}\t测试集正确率:{:.3f}'
+                print('训练周期: {} [{:.0f}%]\tLoss: {:.6f}\t训练集正确率: {:.3f}\t验证集正确率:{:.3f}'
                     .format(epoch+1, 100*(epoch+1)/config.EPOCHS, loss.item(), 
                             100*train_r[0]/train_r[1],
                             100*val_r[0]/val_r[1]))
@@ -66,7 +65,6 @@ def test(model, test_loader):
             test_loss += F.nll_loss(output, y, reduction='sum').item()
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(y.view_as(pred)).sum().item()
-
     test_loss /= len(test_loader.dataset)
     print('\ntest loss={:.4f}, accuracy={:.4f}\n'.format(test_loss, float(correct) / len(test_loader.dataset)))
 
