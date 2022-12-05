976f4ed43889c1c59c4369c71a160183a2200928
tai <t385633933@163.com>
2022-12-05 08:48:07 +0800

    null

--- openCV/faceDemo/config.py
+++ openCV/faceDemo/config.py
@@ -3,10 +3,11 @@ import os
 配置文件
 '''
 
-NUM_WORKERS=1 # 几个线程
-BATCH_SIZE=20 # 每次迭代使用多少个样本
-EPOCHS=20     # 训练的总循环周期
-IMG_SIZE=64  # 输入图像尺寸
+NUM_WORKERS=1   # 几个线程
+TRAIN_SCALE=0.8 # 训练集所占比例
+BATCH_SIZE=20   # 每次迭代使用多少个样本
+EPOCHS=20       # 训练的总循环周期
+IMG_SIZE=64     # 输入图像尺寸
 DEFAULT_MODEL="face_modle.pkl" # 模型名称
 
 # 工作区文件夹路径
@@ -15,7 +16,7 @@ PROJECT_PATH = os.path.abspath(
 
 # 训练集数据集
 DATA_TRAIN = os.path.join(PROJECT_PATH, "data/train")
-# 测试集数据 用于评估模型的准确率,不用于模型构建过程，否则会导致过渡拟合。
-DATA_TEST = os.path.join(PROJECT_PATH, "data/test")
+# 验证集数据
+DATA_VALIDATION = os.path.join(PROJECT_PATH, "data/test")
 # 模型保存地址
 DATA_MODEL = os.path.join(PROJECT_PATH, "data/model")
--- openCV/faceDemo/data_pretreatment.py
+++ openCV/faceDemo/data_pretreatment.py
@@ -2,6 +2,7 @@ import config
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+import torch
 
 '''
 图片预处理
@@ -22,24 +23,39 @@ def get_transform():
 
 def get_dataset(batch_size=10, num_workers=config.NUM_WORKERS):
     data_transform = get_transform()
+    # # 图片
+    # dataset = ImageFolder(root=config.DATA_TRAIN, 
+    #                             transform=data_transform)
     # 训练集图片
     train_dataset = ImageFolder(root=config.DATA_TRAIN, 
                                 transform=data_transform)
-    # 测试集图片
-    test_dataset = ImageFolder(root=config.DATA_TEST, 
-                                transform=data_transform)
+    # # 验证集图片
+    # test_dataset = ImageFolder(root=config.DATA_TEST, 
+    #                             transform=data_transform)
     # 训练数据集的加载器，自动将数据切分成批，顺序随机打乱
-    train_loader = DataLoader(dataset=train_dataset,
-                                batch_size=batch_size,
-                                shuffle=True,
-                                num_workers=num_workers)
-    test_loader = DataLoader(dataset=test_dataset, 
-                            batch_size=batch_size, 
-                            shuffle=True, 
-                            num_workers=num_workers)
+    # train_loader = DataLoader(dataset=train_dataset,
+    #                             batch_size=batch_size,
+    #                             shuffle=True,
+    #                             num_workers=num_workers)
+    # test_loader = DataLoader(dataset=test_dataset, 
+    #                         batch_size=batch_size, 
+    #                         shuffle=True, 
+    #                         num_workers=num_workers)
+
+    train_size = int(config.TRAIN_SCALE * len(train_dataset))
+    validation_size = (len(train_dataset) - train_size) // 2
+    test_size = len(train_dataset) - validation_size
+    train_dataset, test_dataset = torch.utils.data.random_split(train_dataset, [train_size, test_size])
+
+    train_dataset=train_dataset.dataset#这行很重要
+    for images, labels in train_dataset.imgs:
+        print(images)
+        print(labels)
+
     print(len(train_dataset))
     print(train_dataset.class_to_idx) #查看子文件夹与标签的映射
-    return train_loader, test_loader
+    # return train_loader, test_loader
 
 if __name__=="__main__":
-    train_loader, test_loader = get_dataset()
+    # train_loader, test_loader = get_dataset()
+    get_dataset()
